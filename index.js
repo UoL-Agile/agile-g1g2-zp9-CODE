de3cf8183a0d82db77919f88c25a87086233d24f
John Gerhardt <jag24@student.london.ac.uk>
2021-01-24 20:15:20 +0100

    Update index.js
    Added 3 other routes for the main functionalities in our MVP

--- index.js
+++ index.js
@@ -8,6 +8,23 @@ app.get('/whichWeekAreWe', (req, res) => {
    res.send(String(Math.ceil((((new Date() - onejan) / 86400000) + onejan.getDay()+1)/7)));
 });
 
+app.get('/displayMyModuleGrades', (req, res) => {
+   var myGrades = ['72%','34%','56%','89%','90%','72%']
+   res.send(JSON.stringify(myGrades));
+});
+
+
+app.get('/displayMyModuleDeadlines', (req, res) => {
+    var myDeadlines = ['01/01/2021 - Mid Term 1','01/02/2021 - Mid Term 2','01/03/2021 - Mid Term 3','01/04/2021 - Mid Term 4','01/05/2021 - Mid Term ','01/06/2021 - Mid Term 6']
+  res.send(JSON.stringify(myDeadlines))
+});
+
+
+app.get('/displayMyModuleProgress', (req, res) => {
+   var myProgress = ['SDD - 85%','Agile - 58%','Computer Security - 20%']
+  res.send(JSON.stringify(myProgress))
+});
+
 
 //===============PORT and SERVER =================
 var port = process.env.PORT || 3000; // need to get the port that Heroku gives us
