7c825f27ca0b4983ba6762380732e4730e8e5df9
Sharene Lau <sharenelau21@outlook.com>
2021-01-30 17:45:02 +0800

    add shortcut

--- index.js
+++ index.js
@@ -121,6 +121,45 @@ slack_app.command('/prototype-help', async ({ command, ack,respond}) => {
    }
    );
  });
+
+ // The open_modal shortcut opens a plain old modal
+slack_app.shortcut('prototype_uol_shortcut', async ({ shortcut, ack,client }) => {
+  try {
+    // Acknowledge shortcut request
+    await ack();
+    // Call the views.open method using one of the built-in WebClients
+    const workspaceid = "TDT1N1BUG";
+    const result = await client.views.open({
+      trigger_id: shortcut.trigger_id,
+      view: {
+        type: "modal",
+        title: {
+          type: "plain_text",
+          text: "Prototype"
+        },
+        close: {
+          type: "plain_text",
+          text: "Close"
+        },
+        blocks: [
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "Visit " + `<slack://app?team=${workspaceid}&id=${process.env.SLACK_APP_ID}|*Prototype*>`+
+               " bot now to get: \n • current week \n • assignment deadline \n • module grades"
+            }
+          },
+        ]
+      }
+    });
+
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
  
 //=============== The routes =================
 
