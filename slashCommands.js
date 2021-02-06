81d30ae74e731b4e67180e1e3f26611c02d2b6e4
Maksim Grinberg <maxim.1229@yandex.ru>
2021-02-06 21:37:12 +0300

    tests

--- slashCommands.js
+++ slashCommands.js
@@ -147,21 +147,12 @@ module.exports = function(slack_app) {
     
     // Action listener: current week
     slack_app.action('/button-action', async ({
-        action,
         ack,
         respond
     }) => {
         // Acknowledge command request
         await ack();
         await respond({
-            "blocks": [{
-                "type": "section",
-                "text": {
-                    "type": "plain_text",
-                    "text": "Current week :date: : " + getCurrentWeek(true),
-                    "emoji": true
-                }
-            }, ]
         });
     });
     
